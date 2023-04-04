8fc2940214640c763707855440f05bcc389194d4
4ntl3R <71680575+4ntl3R@users.noreply.github.com>
2023-04-04 17:36:49 +0600

    Update IHittable.cs
    Added target property

--- Assets/IdleRPG/Scripts/Runtime/Interfaces/IHittable.cs
+++ Assets/IdleRPG/Scripts/Runtime/Interfaces/IHittable.cs
@@ -1,10 +1,12 @@
 using System;
+using UnityEngine;
 
 namespace IdleRPG.Scripts.Runtime.Interfaces
 {
     public interface IHittable
     {
         event Action<float> OnHitReceived;
+        Transform Target { get; }
         void ReceiveHit(float hitPower);
     }
 }
