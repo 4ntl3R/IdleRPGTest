cdd8ced4f7747d78d4b86b6a2da33aefe40c8014
4ntl3R <71680575+4ntl3R@users.noreply.github.com>
2023-04-04 21:02:26 +0600

    Update IHittable.cs
    Added reset health event

--- Assets/IdleRPG/Scripts/Runtime/Interfaces/IHittable.cs
+++ Assets/IdleRPG/Scripts/Runtime/Interfaces/IHittable.cs
@@ -6,6 +6,7 @@ namespace IdleRPG.Scripts.Runtime.Interfaces
     public interface IHittable
     {
         event Action<float> OnHitReceived;
+        event Action OnHealthReset;
         Transform Target { get; }
         void ReceiveHit(float hitPower);
     }
