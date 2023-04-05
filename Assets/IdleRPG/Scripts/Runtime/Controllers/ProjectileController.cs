3c1813e7c9725b77765cdaf7ef0a4778416b21dc
4ntl3R <71680575+4ntl3R@users.noreply.github.com>
2023-04-05 19:40:40 +0600

    Added projectile classes

---
+++ Assets/IdleRPG/Scripts/Runtime/Controllers/ProjectileController.cs
@@ -0,0 +1,34 @@
+using System;
+using IdleRPG.Scripts.Runtime.Models;
+using IdleRPG.Scripts.Runtime.Views;
+
+namespace IdleRPG.Scripts.Runtime.Controllers
+{
+    public class ProjectileController : IDisposable
+    {
+        private MovingModel _movingModel;
+        private ProjectileView _projectileView;
+
+        public ProjectileController(MovingModel movingModel, ProjectileView projectileView)
+        {
+            _movingModel = movingModel;
+            _projectileView = projectileView;
+            SubscribeEvents();
+        }
+
+        public void Dispose()
+        {
+            UnsubscribeEvents();
+        }
+        
+        private void SubscribeEvents()
+        {
+            _movingModel.OnTargetReached += _projectileView.ResolveHit;
+        }
+
+        private void UnsubscribeEvents()
+        {
+            _movingModel.OnTargetReached -= _projectileView.ResolveHit;
+        }
+    }
+}
---
+++ Assets/IdleRPG/Scripts/Runtime/Views/ProjectileView.cs
@@ -0,0 +1,78 @@
+using System;
+using IdleRPG.Scripts.Runtime.Controllers;
+using IdleRPG.Scripts.Runtime.Interfaces;
+using IdleRPG.Scripts.Runtime.Managers;
+using IdleRPG.Scripts.Runtime.Models;
+using UnityEngine;
+
+namespace IdleRPG.Scripts.Runtime.Views
+{
+    public class ProjectileView : MonoBehaviour, IMovable
+    {
+        public event Action<Vector3, Vector3> OnResetPositions;
+        public event Action<ProjectileView> OnHit;
+
+        [SerializeField] 
+        private Transform visualization;
+
+        [SerializeField]
+        private float speed = 10f;
+
+        [SerializeField] 
+        private float targetReach = 0.05f;
+
+        private float _damage;
+        
+        private MovingController _movingController;
+        private ProjectileController _projectileController;
+        private MovingModel _movingModel;
+        private IIterator _movingIterator;
+        private IHittable _currentTarget;
+
+        private void Awake()
+        {
+            ManageDependencies();
+        }
+
+        private void OnDestroy()
+        {
+            _movingController.Dispose();
+            _projectileController.Dispose();
+            _movingIterator.StopIterating();
+        }
+
+        public void MoveToPosition(Vector3 position)
+        {
+            transform.position = position;
+        }
+
+        public void LookAt(Vector3 target)
+        {
+            visualization.up = target - transform.position;
+        }
+
+        public void Launch(Vector3 launchPosition, ITargetProvider target, float damage)
+        {
+            _currentTarget = target.GetTarget();
+            _damage = damage;
+            OnResetPositions?.Invoke(launchPosition, _currentTarget.Target.position);
+        }
+
+        public void ResolveHit()
+        {
+            if (_currentTarget.Target.gameObject.activeInHierarchy)
+            {
+                _currentTarget.ReceiveHit(_damage);
+            }
+            OnHit?.Invoke(this);
+        }
+        
+        private void ManageDependencies()
+        {
+            _movingModel = new MovingModel(speed, targetReach);
+            _movingIterator = new SingleCoroutineManager(this);
+            _movingController = new MovingController(_movingModel, this, _movingIterator);
+            _projectileController = new ProjectileController(_movingModel, this);
+        }
+    }
+}
